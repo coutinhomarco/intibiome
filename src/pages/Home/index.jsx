5c090ea4554614495d25dfe4a1113b3dcd596731
marco <marquinco@outlook.com>
2023-01-29 22:33:51 -0300

    use picture with sources to show different imgs on different resolutions

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,17 +6,26 @@ import Articles from '../../components/Articles';
 import Footer from '../../components/Footer';
 import './index.scss';
 import bannerHero from '../../assets/Home/banner-hero.png';
+import bannerHeroMobile from '../../assets/Home/banner-hero-mobile.png';
+
 import secondBannerHero from '../../assets/Home/Asset 22.png';
+import secondBannerHeroMobile from '../../assets/Home/banner-intibiome-05.png';
 
 export default function Home() {
   return (
     <div className="home">
       <Header />
-      <img role="presentation" alt="Demonstration of the products available" src={bannerHero} />
+      <picture>
+        <source media="(max-width: 500px)" srcSet={bannerHeroMobile} />
+        <img role="presentation" alt="Demonstration of the products available" src={bannerHero} />
+      </picture>
       <main>
         <AboutUs />
         <OurProducts />
-        <img loading="lazy" alt="care for intimate microbiome" src={secondBannerHero} />
+        <picture>
+          <source media="(max-width: 500px)" srcSet={secondBannerHeroMobile} />
+          <img loading="lazy" alt="care for intimate microbiome" src={secondBannerHero} />
+        </picture>
         <Articles />
       </main>
       <Footer />
