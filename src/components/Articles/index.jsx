039f44439a1d36e1a8523084dceb3d9228c68925
marco <marquinco@outlook.com>
2023-01-29 23:32:22 -0300

    finish adjusts to mobile version

--- src/components/Articles/index.jsx
+++ src/components/Articles/index.jsx
@@ -7,23 +7,25 @@ import './index.scss';
 
 export default function index() {
   return (
-    <section className="articles">
-      <h1>keep up to date with our discoveries</h1>
-      <div className="articles-container">
-        <div id="article1" className="article">
-          <img loading="lazy" alt="article" src={article1} />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <section className="outer-article-container">
+      <h1 id="articles-h1">keep up to date with our discoveries</h1>
+      <section className="articles">
+        <div className="articles-container">
+          <div id="article1" className="article">
+            <img loading="lazy" alt="article" src={article1} />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+          <div id="article2" className="article">
+            <img loading="lazy" alt="article" src={article2} />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+          <div id="article3" className="article">
+            <img loading="lazy" alt="article" src={article3} />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
         </div>
-        <div id="article2" className="article">
-          <img loading="lazy" alt="article" src={article2} />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div id="article3" className="article">
-          <img loading="lazy" alt="article" src={article3} />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-      <button type="button">see more</button>
+      </section>
+      <button id="see-more-btn" type="button">see more</button>
     </section>
   );
 }
